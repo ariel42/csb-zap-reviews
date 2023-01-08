5641ca361532d3e81adb26a2c54f1d605c73e5cd
Ariel Ben-Ami <arielb@d.co.il>
2023-01-08 09:18:44 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
@@ -6,9 +6,8 @@ export default function App() {
   const [siteId, setSiteId] = useState();
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox!!</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <h1>Zap Reviews</h1>
+    </>
   );
 }
