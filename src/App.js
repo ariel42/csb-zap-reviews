66d324994cf9d80e39e71f491f6559d560e0eeb5
Ariel Ben-Ami <arielb@d.co.il>
2023-01-08 10:42:07 +0000

    initial zap-review component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,36 @@ export default function App() {
   return (
     <>
       <h1>Zap Reviews</h1>
+      <div>
+        <label htmlFor="customerId">Zap Customer Id:</label> &nbsp;
+        <input
+          name="customerId"
+          id="customerId"
+          type="text"
+          value={customerId}
+          onChange={(e) => setCustomerId(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="siteId">Site:</label> &nbsp;
+        <select
+          name="siteId"
+          id="siteId"
+          onChange={(e) => setSiteId(e.target.value)}
+          value={siteId}
+          defaultValue=""
+        >
+          <option value="" disabled hidden>
+            נא לבחור אתר
+          </option>
+          <option value="5">דפי זהב</option>
+          <option value="3">רסט</option>
+          <option value="2">מתחתנים</option>
+        </select>
+      </div>
+      <div>
+        <zap-reviews customer-id={customerId} site-id={siteId}></zap-reviews>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+if (typeof ZapReviewsElement === "undefined") {
+  const script = document.createElement("script");
+  script.src = `${location.origin}/zap-reviews.js?${Math.random()}`;
+  document.body.appendChild(script);
+}
+
 root.render(
   <StrictMode>
     <App />
